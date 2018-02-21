d710826d5cde8e7eddf36da32e64bba8c49db3ed
Sergio Perez <dark07perez@gmail.com>
2018-02-21 18:49:52 -0500

    primer commit lab5

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "======================== CONFIGURACION GIT ======================== "
+ git config --global user.name "Sergio Perez"
+echo "======================== SERGIO PEREZ -> GIT ======================== "
+
+ git config --global user.email dark07perez@gmail.com
+echo "========================  EMAIL USER - GIT ======================== "
