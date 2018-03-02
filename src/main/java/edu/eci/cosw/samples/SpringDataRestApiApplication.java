e3f424bab6b6cd020319db6a8f472600609c88fb
Sergio Perez <dark07perez@gmail.com>
2018-03-02 06:10:11 -0500

    src/main/resources, configuracion BD

--- src/main/java/edu/eci/cosw/samples/SpringDataRestApiApplication.java
+++ src/main/java/edu/eci/cosw/samples/SpringDataRestApiApplication.java
@@ -2,7 +2,11 @@ package edu.eci.cosw.samples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("edu.eci.cosw.paquete1.paquete2.paquete3")
+@EntityScan("edu.eci.cosw.jpa.paquete1.paquete2.paquete3")
 @SpringBootApplication
 public class SpringDataRestApiApplication {
 
